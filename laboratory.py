8f9f991c5687cee1e01f76935315775e152472d6
pauloyza <andrepaulo@id.uff.br>
2022-07-27 03:47:55 -0300

    v0.2 First greedy step implemented (withou optiization with a local search yet)

--- laboratory.py
+++ laboratory.py
@@ -14,8 +14,25 @@ print(sabedor)
 matriz = np.loadtxt('five_d.txt') 
 #print(matriz)
 matriz[matriz == 0] = np.nan    
-print(matriz)
-print(np.argsort(matriz))
+#print(matriz)
+#print(np.argsort(matriz))
 #x = np.arange(24).reshape((2, 3, 4))
 #print(x)
 #print(np.argm()))
+
+#city_all = len(matriz[0])
+#print(city_all)
+#cities = np.arange(0, city_all)
+#print(cities)
+
+array_uai = np.arange(1,5)
+array_normal = np.arange(1,6)
+print(array_uai)
+print(array_normal)
+
+array_bool = np.in1d(array_normal, array_uai)
+print(array_bool)
+print(np.argwhere(array_bool == False))
+array_uai[-1]
+
+print(np.any(array_uai == 3))
